fbba24ecf91e1b16448d257c70ab5fd0ae1d7f0d
noah <noah@supergene.co>
2023-05-24 19:18:45 +0900

    test6

--- app/src/main/java/com/wing/tree/dwds/smaple/MainActivity.kt
+++ app/src/main/java/com/wing/tree/dwds/smaple/MainActivity.kt
@@ -20,7 +20,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    println("dorte2222sdfsf")
+                    println("dorte2222sdsfdsfdfsf")
                 }
             }
         }

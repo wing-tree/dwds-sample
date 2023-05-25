4baa964b551645e30e55629d62d4ef475bde9c93
noah <noah@supergene.co>
2023-05-25 10:38:28 +0900

    d-test2

--- app/src/main/java/com/wing/tree/dwds/smaple/MainActivity.kt
+++ app/src/main/java/com/wing/tree/dwds/smaple/MainActivity.kt
@@ -14,14 +14,15 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
+        var King = "king"
+
         setContent {
             DwdsSampleTheme {
                 // A surface container using the 'background' color from the theme
                 Surface(
                     modifier = Modifier.fillMaxSize(),
-                    color = MaterialTheme.colorScheme.background
+                    color = MaterialTheme.colorScheme.background,
                 ) {
-
                     println("qqqqq")
                     println("kingking22")
                     Text(text = "dev")
